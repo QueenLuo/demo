99e797bd0d4a5d83d8f9242c2cb6a47d5dbb4775
QueenLuo <gklife12@gklife-12deimac.local>
2016-05-19 10:43:06 +0800

    first

--- 按钮旋转/ViewController.m
+++ 按钮旋转/ViewController.m
@@ -7,8 +7,28 @@
 //
 
 #import "ViewController.h"
+#import "PlaceholderTextView.h"
+
+#define DEGREES_TO_RADIANS(angle) ((angle)/180.0 * M_PI)
+
+typedef enum {
+    RotateStateStop,
+    RotateStateRunning,
+}RotateState;
 
 @interface ViewController ()
+{
+    //旋转角度
+    CGFloat imageViewAngle;
+    //旋转ImageView
+    UIImageView *imageView;
+    //旋转状态
+    RotateState rotateState;
+}
+
+@property (weak, nonatomic) IBOutlet UISwitch *sw;
+@property (weak, nonatomic) IBOutlet PlaceholderTextView *textview;
+
 
 @end
 
@@ -16,12 +36,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.title = @"按钮旋转测试";
+    [self buildBarButtonItem];
+    
+    self.sw.offImage = [UIImage imageNamed:@"12.png"];
+    self.sw.tintColor = [UIColor orangeColor];
+    
+    self.textview.placeholder = @"请输入姓名";
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)buildBarButtonItem{
+    imageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"icon.png"]];
+    imageView.autoresizingMask = UIViewAutoresizingNone;
+    imageView.contentMode = UIViewContentModeScaleToFill;
+    imageView.bounds = CGRectMake(0, 0, 40, 40);
+    //设置视图为圆形
+    imageView.layer.masksToBounds = YES;
+    imageView.layer.contentsScale = 20.0f;
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(0, 0, 40, 40);
+    [button addSubview:imageView];
+    [button addTarget:self action:@selector(animate) forControlEvents:UIControlEventTouchUpInside];
+    imageView.center = button.center;
+    
+    UIBarButtonItem *barItem = [[UIBarButtonItem alloc]initWithCustomView:button];
+    
+    self.navigationItem.rightBarButtonItem = barItem;
+}
+
+-(void)animate{
+    //改变imageview的旋转状态
+    if (rotateState == RotateStateStop) {
+        rotateState = RotateStateRunning;
+        [self rotateAnimate];
+    }else{
+        rotateState = RotateStateStop;
+    }
+    
+    NSLog(@"%@",self.textview.text);
+}
+
+-(void)rotateAnimate{
+    imageViewAngle += 50;
+    [UIView animateWithDuration:0.5 delay:0.0 options:UIViewAnimationOptionCurveLinear animations:^{
+        imageView.transform = CGAffineTransformMakeRotation(DEGREES_TO_RADIANS(imageViewAngle));
+    } completion:^(BOOL finished) {
+        if (rotateState == RotateStateRunning) {
+            [self rotateAnimate];
+        }
+    }];
 }
 
 @end
